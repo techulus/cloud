776f6a2e1997acfb978ab5740af2b823a448c7f2
Arjun Komath <arjun@hey.com>
2025-03-11 12:58:42 +1100

    Setup temporal workflow

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,34 @@
+import { WorkflowClient } from "@temporalio/client";
+import { Connection } from "@temporalio/client";
+import { randomUUIDv7 } from "bun";
 import { Hono } from "hono";
 
 const app = new Hono();
 
 app.get("/", (c) => {
-	return c.text("Hello Hono!");
+	return c.json({ message: "Hello!" });
+});
+
+app.post("/deploy", async (c) => {
+	const { image } = await c.req.json();
+
+	const connection = await Connection.connect({
+		address: process.env.TEMPORAL_ADDRESS || "127.0.0.1:7233",
+	});
+	const client = new WorkflowClient({ connection, namespace: "default" });
+
+	const handle = await client.start("deployService", {
+		args: [image],
+		taskQueue: "service-deployment",
+		workflowId: randomUUIDv7(),
+	});
+
+	console.log(
+		`Started Workflow ${handle.workflowId} with RunID ${handle.firstExecutionRunId}`,
+	);
+	console.log(await handle.result());
+
+	return c.json({ message: "Done" });
 });
 
 export default {
