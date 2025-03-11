d1897ed564fc72b1ce9d6d389fc22ebe3541bc80
Arjun Komath <arjun@hey.com>
2025-03-11 22:45:14 +1100

    Add auth and drizzle

---
+++ web/middleware.ts
@@ -0,0 +1,35 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+const publicAppPaths = ["/sign-in", "/terms", "/api/auth"];
+
+export async function middleware(request: NextRequest) {
+	const { pathname } = request.nextUrl;
+
+	const isPublicAppPath = publicAppPaths.some((path) =>
+		pathname.startsWith(path),
+	);
+	if (isPublicAppPath || pathname === "/") {
+		return NextResponse.next();
+	}
+
+	const session = getSessionCookie(request);
+	if (session && pathname === "/sign-in") {
+		return NextResponse.redirect(new URL("/start", request.nextUrl.href));
+	}
+
+	if (!session) {
+		return NextResponse.redirect(
+			new URL(
+				`/sign-in?redirectTo=${encodeURIComponent(request.nextUrl.href)}`,
+				request.url,
+			),
+		);
+	}
+
+	return NextResponse.next();
+}
+
+export const config = {
+	matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
