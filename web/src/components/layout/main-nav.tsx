c7aa58337f7a23db4e538d6ee9da9688a454e4de
Arjun Komath <arjun@hey.com>
2025-03-12 23:36:39 +1100

    Create workspace

--- web/src/components/layout/main-nav.tsx
+++ web/src/components/layout/main-nav.tsx
@@ -1,85 +1,103 @@
-"use client"
+"use client";
 
-import { Avatar } from '@/components/ui/avatar'
+import { Avatar } from "@/components/ui/avatar";
 import {
-  Dropdown,
-  DropdownButton,
-  DropdownDivider,
-  DropdownItem,
-  DropdownLabel,
-  DropdownMenu,
-} from '@/components/ui/dropdown'
-import { Navbar, NavbarDivider, NavbarItem, NavbarLabel, NavbarSection, NavbarSpacer } from '@/components/ui/navbar'
+	Dropdown,
+	DropdownButton,
+	DropdownDivider,
+	DropdownItem,
+	DropdownLabel,
+	DropdownMenu,
+} from "@/components/ui/dropdown";
+import {
+	Navbar,
+	NavbarDivider,
+	NavbarItem,
+	NavbarLabel,
+	NavbarSection,
+	NavbarSpacer,
+} from "@/components/ui/navbar";
 import { signOut } from "@/lib/auth-client";
 import {
-  ArrowRightStartOnRectangleIcon,
-  ChevronDownIcon,
-  Cog8ToothIcon,
-  LightBulbIcon,
-  ShieldCheckIcon,
-  UserIcon,
-} from '@heroicons/react/16/solid'
-import { InboxIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
-import TeamsNav from './teams-nav';
-import { useRouter } from 'next/navigation';
+	ArrowRightStartOnRectangleIcon,
+	ChevronDownIcon,
+	Cog8ToothIcon,
+	LightBulbIcon,
+	ShieldCheckIcon,
+	UserIcon,
+} from "@heroicons/react/16/solid";
+import TeamsNav from "./teams-nav";
+import { useRouter } from "next/navigation";
+import type { User } from "better-auth";
+import type { Organization } from "@/lib/user";
+export function MainNav({
+	navItems,
+	user,
+	organizations,
+	activeOrganization,
+}: {
+	navItems: { label: string; url: string }[];
+	user: User;
+	organizations: Organization[];
+	activeOrganization: Organization;
+}) {
+	const router = useRouter();
 
-export function MainNav({ navItems }: { navItems: { label: string; url: string }[] }) {
-    const router = useRouter();
-
-    return (<Navbar>
-          <Dropdown>
-            <DropdownButton as={NavbarItem} className="max-lg:hidden">
-              <Avatar src="/logo.png" />
-              <NavbarLabel>Techulus</NavbarLabel>
-              <ChevronDownIcon />
-            </DropdownButton>
-            <TeamsNav />
-          </Dropdown>
-          <NavbarDivider className="max-lg:hidden" />
-          <NavbarSection className="max-lg:hidden">
-            {navItems.map(({ label, url }) => (
-              <NavbarItem key={label} href={url}>
-                {label}
-              </NavbarItem>
-            ))}
-          </NavbarSection>
-          <NavbarSpacer />
-          <NavbarSection>
-            {/* <NavbarItem href="/inbox" aria-label="Inbox">
-              <InboxIcon />
-            </NavbarItem> */}
-            <Dropdown>
-              <DropdownButton as={NavbarItem}>
-                <Avatar src="/logo.png" square />
-              </DropdownButton>
-              <DropdownMenu className="min-w-64" anchor="bottom end">
-                <DropdownItem href="/my-profile">
-                  <UserIcon />
-                  <DropdownLabel>My profile</DropdownLabel>
-                </DropdownItem>
-                <DropdownItem href="/settings">
-                  <Cog8ToothIcon />
-                  <DropdownLabel>Settings</DropdownLabel>
-                </DropdownItem>
-                <DropdownDivider />
-                <DropdownItem href="/privacy-policy">
-                  <ShieldCheckIcon />
-                  <DropdownLabel>Privacy policy</DropdownLabel>
-                </DropdownItem>
-                <DropdownItem href="/share-feedback">
-                  <LightBulbIcon />
-                  <DropdownLabel>Share feedback</DropdownLabel>
-                </DropdownItem>
-                <DropdownDivider />
-                <DropdownItem onClick={async () => {
-                  await signOut();
-                  router.replace("/");
-                }}>
-                  <ArrowRightStartOnRectangleIcon />
-                  <DropdownLabel>Sign out</DropdownLabel>
-                </DropdownItem>
-              </DropdownMenu>
-            </Dropdown>
-          </NavbarSection>
-        </Navbar>)
+	return (
+		<Navbar>
+			<Dropdown>
+				<DropdownButton as={NavbarItem} className="max-lg:hidden">
+					<Avatar src={activeOrganization.logo} />
+					<NavbarLabel>{activeOrganization.name}</NavbarLabel>
+					<ChevronDownIcon />
+				</DropdownButton>
+				<TeamsNav organizations={organizations} />
+			</Dropdown>
+			<NavbarDivider className="max-lg:hidden" />
+			<NavbarSection className="max-lg:hidden">
+				{navItems.map(({ label, url }) => (
+					<NavbarItem key={label} href={url}>
+						{label}
+					</NavbarItem>
+				))}
+			</NavbarSection>
+			<NavbarSpacer />
+			<NavbarSection>
+				<Dropdown>
+					<DropdownButton as={NavbarItem}>
+						<Avatar src={user.image ?? "/logo.png"} square />
+					</DropdownButton>
+					<DropdownMenu className="min-w-64" anchor="bottom end">
+						<DropdownItem href="/my-profile">
+							<UserIcon />
+							<DropdownLabel>My profile</DropdownLabel>
+						</DropdownItem>
+						<DropdownItem href="/settings">
+							<Cog8ToothIcon />
+							<DropdownLabel>Settings</DropdownLabel>
+						</DropdownItem>
+						<DropdownDivider />
+						<DropdownItem href="/privacy-policy">
+							<ShieldCheckIcon />
+							<DropdownLabel>Privacy policy</DropdownLabel>
+						</DropdownItem>
+						<DropdownItem href="/share-feedback">
+							<LightBulbIcon />
+							<DropdownLabel>Share feedback</DropdownLabel>
+						</DropdownItem>
+						<DropdownDivider />
+						<DropdownItem
+							onClick={async () => {
+								await signOut();
+								router.replace("/");
+							}}
+						>
+							<ArrowRightStartOnRectangleIcon />
+							<DropdownLabel>Sign out</DropdownLabel>
+						</DropdownItem>
+					</DropdownMenu>
+				</Dropdown>
+			</NavbarSection>
+		</Navbar>
+	);
 }
--- web/src/components/layout/teams-nav.tsx
+++ web/src/components/layout/teams-nav.tsx
@@ -1,31 +1,31 @@
-import { Avatar } from "../ui/avatar"
-import { DropdownDivider, DropdownItem, DropdownLabel, DropdownMenu } from "../ui/dropdown"
+import type { Organization } from "@/lib/user";
+import { Avatar } from "../ui/avatar";
 import {
-  Cog8ToothIcon,
-  PlusIcon,
-} from '@heroicons/react/16/solid'
+	DropdownDivider,
+	DropdownItem,
+	DropdownLabel,
+	DropdownMenu,
+} from "../ui/dropdown";
+import { PlusIcon } from "@heroicons/react/16/solid";
 
-export default function TeamsNav() {
-    return (
-    <DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
-      <DropdownItem href="/teams/1/settings">
-        <Cog8ToothIcon />
-        <DropdownLabel>Settings</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="/teams/1">
-        <Avatar slot="icon" src="/logo.png" />
-        <DropdownLabel>Techulus</DropdownLabel>
-      </DropdownItem>
-      <DropdownItem href="/teams/2">
-        <Avatar slot="icon" initials="WC" className="bg-purple-500 text-white" />
-        <DropdownLabel>Arjun Komath</DropdownLabel>
-      </DropdownItem>
-      <DropdownDivider />
-      <DropdownItem href="/teams/create">
-        <PlusIcon />
-        <DropdownLabel>New team&hellip;</DropdownLabel>
-      </DropdownItem>
-    </DropdownMenu>
-  )
+export default function TeamsNav({
+	organizations,
+}: {
+	organizations: Organization[];
+}) {
+	return (
+		<DropdownMenu className="min-w-80 lg:min-w-64" anchor="bottom start">
+			{(organizations ?? []).map((organization) => (
+				<DropdownItem key={organization.id} href={`/teams/${organization.id}`}>
+					<Avatar slot="icon" src={organization.logo} />
+					<DropdownLabel>{organization.name}</DropdownLabel>
+				</DropdownItem>
+			))}
+			<DropdownDivider />
+			<DropdownItem href="/start">
+				<PlusIcon />
+				<DropdownLabel>New workspace</DropdownLabel>
+			</DropdownItem>
+		</DropdownMenu>
+	);
 }
